41327161c9a904c7b94dceb49454003d64fd34c6
Chad Glazier <chadglazier@outlook.com>
2024-11-27 17:07:51 -0800

    updated readme, tested the graph loading

--- src/main.R
+++ src/main.R
@@ -1,2 +1,7 @@
-source("libraries.R")
+source("src/libraries.R")
 
+y2018_edges <- read_csv("data/2018_top_10000_pagerank_edges.csv")
+y2018_nodes <- read_csv("data/2018_top_10000_pagerank_nodes.csv")
+
+y2018_edges |> glimpse() |> print()
+y2018_nodes |> glimpse() |> print()
