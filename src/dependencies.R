4d9bbf752a7f9142154c4b6bd83e8b441d0c8559
Chad Glazier <chadglazier@outlook.com>
2024-12-06 21:06:58 -0800

    dependencies file

---
+++ src/dependencies.R
@@ -0,0 +1,8 @@
+#' This file can be run to install all libraries that are used throughout
+#' the project.
+
+install.packages("tidyverse")
+install.packages("igraph")
+install.packages("ggplot2")
+install.packages("purrr")
+install.packages("dplyr")
